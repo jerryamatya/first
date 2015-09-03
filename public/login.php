8b5dfad5bffd7902bacdfe80f56840a0fc2ceb0f
jerryamatya <lna.amatya@gmail.com>
2015-09-03 13:34:18 +0545

    login page added

---
+++ public/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+</head>
+<body>
+<label>Username</label>
+<input type="text" name="username">
+<label>Password</label>
+<input type="password" name="password">
+<input type="submit" value="login">
+</body>
+</html>
